02a493e2f16e356b15f0e9ed3864e73b56dea072
nandinishah8 <nandinishah8501@gmail.com>
2023-07-10 18:38:36 +0530

    feature_homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './user/signup/signup.component';
 import { SigninComponent } from './user/signin/signin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent, SigninComponent],
+  declarations: [AppComponent, SignupComponent, SigninComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
