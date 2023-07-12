d09eaa934ea09c5b6e3be7aff9eee82c968f8cbf
devanshmehta04 <devanshmehta72@gmail.com>
2023-07-12 15:24:10 +0530

    sidebar change

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  researches: any[] = [
+    {
+      title: "Research 1",
+      creationDate: "2023-07-1"
+    },
+    {
+      title: "Research 2",
+      creationDate: "2023-07-2"
+    },
+    {
+      title: "Research 3",
+      creationDate: "2023-07-3"
+    },
+    {
+      title: "Research 4",
+      creationDate: "2023-07-4"
+    },
+  ];
+  filteredResearches :string[]=[];
   searchQuery: string = '';
 
   search(): void {
