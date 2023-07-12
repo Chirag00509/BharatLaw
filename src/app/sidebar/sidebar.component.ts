b9a15c021a33d4618798d77cf242ea3a9ef23319
Chirag00509 <chiraglabha31@gmail.com>
2023-07-12 17:24:05 +0530

    change and add some css

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,35 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  researches: any[] = [
-    {
-      title: "Research 1",
-      creationDate: "2023-07-1"
-    },
-    {
-      title: "Research 2",
-      creationDate: "2023-07-2"
-    },
-    {
-      title: "Research 3",
-      creationDate: "2023-07-3"
-    },
-    {
-      title: "Research 4",
-      creationDate: "2023-07-4"
-    },
-  ];
-  filteredResearches :string[]=[];
-  searchQuery: string = '';
+  researches: any[] = [];
 
-  search(): void {
-    // Perform search operation
-    // Example: log the search query
-    console.log('Search query:', this.searchQuery);
-  }
-  research: string[] = ['Research 1', 'Research 2', 'Research 3', 'Reasearch 4'];
-  openAddResearchModal() {
-    console.log('Add new Research');
 }
-}
-  
+
