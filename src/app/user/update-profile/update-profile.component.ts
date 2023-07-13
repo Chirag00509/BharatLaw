518b8263570675cacfeab121067c988b0dd7a31d
nandinishah8 <nandinishah8501@gmail.com>
2023-07-13 14:42:20 +0530

    changes update-profile

--- src/app/user/update-profile/update-profile.component.ts
+++ src/app/user/update-profile/update-profile.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-update-profile',
   templateUrl: './update-profile.component.html',
-  styleUrls: ['./update-profile.component.css']
+  styleUrls: ['./update-profile.component.css'],
 })
 export class UpdateProfileComponent implements OnInit {
-
-  updateProfile!: FormGroup
+  updateProfile!: FormGroup;
 
   producList: any;
+  emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-  constructor(private formBuilder: FormBuilder, private userservice: UserService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userservice: UserService
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -24,6 +32,7 @@ export class UpdateProfileComponent implements OnInit {
     this.updateProfile = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
+      email: ['', Validators.required],
       contactDetails: ['', Validators.required],
       organization: ['', Validators.required],
     });
@@ -34,32 +43,31 @@ export class UpdateProfileComponent implements OnInit {
   }
 
   getFormDetails() {
-    let token = localStorage.getItem("token");
-    this.userservice.getUser().subscribe((users: any[]) => {
-      let user = users.find(u => u.actionToken == token);
+    let token = localStorage.getItem('token');
+    // this.userservice.getUser().subscribe((users: any[]) => {
+    //   let user = users.find((u) => u.actionToken == token);
 
+    this.userservice.getDetailsByToken(token).subscribe((user) => {
       this.producList = user;
 
       this.updateProfile.patchValue({
         firstName: this.producList.firstName,
         lastName: this.producList.lastName,
+        email: this.producList.email,
         contactDetails: this.producList.contactDetails,
-        organization: this.producList.organization
-      })
-    })
+        organization: this.producList.organization,
+      });
+    });
   }
 
   updateForm(data: any) {
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem('token');
     this.userservice.getDetailsByToken(token).subscribe((user) => {
-      data.email = user.email;
       data.password = user.password;
       data.actionToken = token;
       data.Id = user.id;
 
       this.userservice.updateProfile(user.id, data).subscribe();
-    })
-
+    });
   }
-
 }
